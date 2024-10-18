bbb534bbf9ed9a4f9ba66264ef97706de29c3a63
David Murdoch <dsmurdoch357@gmail.com>
2024-10-18 14:39:02 +0100

    chore(next.js): upgrade next.js to v15-rc-1

--- src/app/(auth-pages)/sign-in/page.tsx
+++ src/app/(auth-pages)/sign-in/page.tsx
@@ -5,9 +5,11 @@ import { SubmitButton } from "~/components/submit-button";
 import { Input } from "~/components/ui/input";
 import { Label } from "~/components/ui/label";
 
-export default function Login({ searchParams }: { searchParams: Message }) {
+export default async function Login(props: { searchParams: Promise<Message> }) {
+  const searchParams = await props.searchParams;
   return (
-    <form className="flex min-w-64 flex-1 flex-col">
+    // eslint-disable-next-line @typescript-eslint/no-misused-promises
+    <form className="flex min-w-64 flex-1 flex-col" action={signInAction}>
       <h1 className="text-2xl font-medium">Sign in</h1>
       <p className="text-sm text-foreground">
         Don't have an account?{" "}
@@ -33,9 +35,7 @@ export default function Login({ searchParams }: { searchParams: Message }) {
           placeholder="Your password"
           required
         />
-        <SubmitButton pendingText="Signing In..." formAction={signInAction}>
-          Sign in
-        </SubmitButton>
+        <SubmitButton pendingText="Signing In...">Sign in</SubmitButton>
         <FormMessage message={searchParams} />
       </div>
     </form>
--- src/app/protected/reset-password/page.tsx
+++ src/app/protected/reset-password/page.tsx
@@ -4,13 +4,16 @@ import { SubmitButton } from "~/components/submit-button";
 import { Input } from "~/components/ui/input";
 import { Label } from "~/components/ui/label";
 
-export default function ResetPassword({
-  searchParams,
-}: {
-  searchParams: Message;
+export default async function ResetPassword(props: {
+  searchParams: Promise<Message>;
 }) {
+  const searchParams = await props.searchParams;
   return (
-    <form className="flex w-full max-w-md flex-col gap-2 p-4 [&>input]:mb-4">
+    <form
+      className="flex w-full max-w-md flex-col gap-2 p-4 [&>input]:mb-4"
+      // eslint-disable-next-line @typescript-eslint/no-misused-promises
+      action={resetPasswordAction}
+    >
       <h1 className="text-2xl font-medium">Reset password</h1>
       <p className="text-sm text-foreground/60">
         Please enter your new password below.
@@ -29,9 +32,7 @@ export default function ResetPassword({
         placeholder="Confirm password"
         required
       />
-      <SubmitButton formAction={resetPasswordAction}>
-        Reset password
-      </SubmitButton>
+      <SubmitButton>Reset password</SubmitButton>
       <FormMessage message={searchParams} />
     </form>
   );
